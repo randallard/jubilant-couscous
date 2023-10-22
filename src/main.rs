54daf7970f13e955f365e692c1ade73535c445ee
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-10-22 09:09:01 -0700

    began implementation of Table, Person, Game, Player, Board, Piece, Row, and Space

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,99 @@
-use std::any::type_name;
+//  People playing a game 
+//  People in the same room
+//      Talking happens in real life
+//  Each person plays on their own device
+//  Bash Dash - get to the other side of the board 
+//      - set your play 
+//          - you can move left, right or up
+//          - you can set a blocker either left or right of your current space
+//
+//      - points awarded for 
+//          - spaces moved towards the other side
+//          - opponent running into your blocker
+//          - leaving the board on the other side
+//
+//      - points taken away for 
+//          - setting a blocker
+//          - running into your oponnent's blocker 
+//
+//      - round ends when
+//          - opponnents collide
+//          - player moves into a space that contains an oponnent's blocker
+//          - player leaves the board on the other side
 
+struct Person {}
+struct Player {}
+struct Piece {}
 struct Space {}
-
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
+struct Row {
+    space : Option<Space>,
 }
-
-struct Grid {
-    spaces : Vec<Space>,
-}
-
-impl Grid {
-    fn has_spaces(&self) -> bool {
-        type_of(self.get_spaces()) == "&alloc::vec::Vec<run_game::Space>"
-    }
-    fn get_spaces(&self) -> &Vec<Space> {
-        &self.spaces
+impl Row {
+    fn has_space(&self) -> bool {
+        match self.space {
+            Some(_) => true,
+            None => false,
+        }
     }
 }
 struct Board {
-    grid : Grid,
+    row : Option<Row>,
 }
-
 impl Board {
-    fn has_grid(&self) -> bool {
-        type_of(&self.get_grid()) == "&&run_game::Grid"
-    }
-    fn get_grid(&self) -> &Grid {
-        &self.grid
+    fn has_row(&self) -> bool {
+        match self.row {
+            Some(_) => true,
+            None => false,
+        }
     }
 }
-
-struct Person {}
+struct Game {
+    players : Vec<Player>,
+    pieces : Vec<Piece>,
+    board : Option<Board>,
+}
+impl Game {
+    fn has_player(&self) -> bool {
+        match self.players.get(0) {
+            Some(_) => true,
+            None => false,
+        }
+    }
+    fn has_piece(&self) -> bool {
+        match self.pieces.get(0) {
+            Some(_) => true,
+            None => false
+        }
+    }
+    fn has_board(&self) -> bool {
+        match self.board {
+            Some(_) => true,
+            None => false
+        }
+    }
+}
 
 struct Table {
     people : Vec<Person>,
+    game : Option<Game>,
 }
 
 impl Table {
     fn exists(&self) -> bool { true }
+    fn has_game(&self) -> bool {
+        match self.game {
+            Some(_) => true,
+            None => false,
+        }
+    }
     fn has_person(&self) -> bool {
          match self.people.get(0) {
+            Some(_) => true,
+            None => false,
+         }
+    }
+    fn has_another_person(&self) -> bool {
+         match self.people.get(1) {
             Some(_) => true,
             None => false,
          }
@@ -48,18 +101,30 @@ impl Table {
 }
 
 fn main() {
-    let table = Table { people : vec![ Person { } ]};
+    let table = Table { 
+        people : vec![ Person{}, Person{} ],
+        game: Some(Game{ 
+            players : vec![ Player {} ],
+            pieces : vec![ Piece {} ],
+            board : Some(Board { 
+                row: Some( Row {
+                    space: Some( Space {} ),
+                } ), 
+            }),
+        }),
+    };
     assert!(table.exists());
     assert!(table.has_person());
-    //assert!(table.has_game());
-    //assert!(game.has_a_player());
-    //assert!(game.has_a_piece());
-    //assert!(game.each_player_is_a_person());
-    //assert!(game.each_player_has_piece());
-    //assert!(game.has_board());
-    let board : Board = Board { grid: Grid { spaces : vec![ Space { } ]  }};
-    assert!(board.has_grid());
-    assert!(board.get_grid().has_spaces());    
-    //assert!(game.each_piece_is_on_the_board());
+    assert!(table.has_another_person());
+    assert!(table.has_game());
+    let game = table.game.expect("table has no game");
+    assert!(game.has_player());
+    assert!(game.has_piece());
+    assert!(game.has_board());
+    // the players piece starts in the middle of their first row of spaces
+    let board = game.board.expect("game has no board");
+    assert!(board.has_row());
+    let row = board.row.expect("board has no row");
+    assert!(row.has_space());
     println!("Hello, Bash Dash!");
 }
